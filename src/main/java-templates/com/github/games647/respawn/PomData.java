88544ec069cdad9ebcf3785f9a11973afa366737
games647 <games647@users.noreply.github.com>
2017-10-19 17:04:52 +0200

    Minor changes

--- src/main/java-templates/com/github/games647/respawn/PomData.java
+++ src/main/java-templates/com/github/games647/respawn/PomData.java
@@ -2,9 +2,9 @@ package com.github.games647.respawn;
 
 class PomData {
 
-    public static final String ARTIFACT_ID = "${project.artifactId}";
-    public static final String NAME = "${project.name}";
-    public static final String VERSION = "${project.version}";
-    public static final String URL = "${project.url}";
-    public static final String DESCRIPTION = "${project.description}";
+    static final String ARTIFACT_ID = "${project.artifactId}";
+    static final String NAME = "${project.name}";
+    static final String VERSION = "${project.version}";
+    static final String URL = "${project.url}";
+    static final String DESCRIPTION = "${project.description}";
 }
